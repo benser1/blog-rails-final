9fb0dcad7bde61c724f59dec2f4345af1cf9cf1f
benser1 <briganenser28@gmail.com>
2016-02-20 12:44:53 -0700

    Fields in comment form required working

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,7 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   accepts_nested_attributes_for :user, reject_if: :all_blank
 
+  validates :name, presence: true
+  validates :content, presence: true
+
 end
